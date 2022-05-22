5c2c58fd8528dff255f363a7ea1c2973573a379d
capmahin <mahinulhuque1996@gmail.com>
2022-05-23 05:47:38 +0600

    destractaring img name price

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-const Service = () => {
+const Service = ({ service }) => {
   return (
     <div class="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
-        <img
-          src="https://api.lorem.space/image/shoes?w=400&h=225"
-          alt="Shoes"
-        />
+        <img src={service.img} alt="Shoes" />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
+        <h2 class="card-title">{service.name}</h2>
+        <h4 class="card-title">{service.price}</h4>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+          <button class="btn btn-primary">Book Now !</button>
         </div>
       </div>
     </div>
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -11,18 +11,21 @@ const Services = () => {
       name: "Engine",
       description: "",
       img: engine,
+      price: 120000,
     },
     {
       _id: 2,
       name: "Spear",
       description: "",
       img: spear,
+      price: 150000,
     },
     {
       _id: 3,
       name: "Wheel",
       description: "",
       img: wheel,
+      price: 110000,
     },
   ];
   return (
@@ -33,7 +36,7 @@ const Services = () => {
         </h2>
         <h2 className="text-4xl">Service we provide</h2>
       </div>
-      <div>
+      <div className="grid sm.grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {services.map((service) => (
           <Service key={service._id} service={service}></Service>
         ))}
