5d5d53ddd08ce7a8fab1b515f7a5754e2345432a
capmahin <mahinulhuque1996@gmail.com>
2022-06-16 04:28:15 +0600

    fetch and  react-query

--- src/Pages/Dashboard/MangeMechanic.js
+++ src/Pages/Dashboard/MangeMechanic.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const MangeMechanic = () => {
+  const { data: mechanics, isLoading } = useQuery("mechanics", () =>
+    fetch("http://localhost:5000/mechanic", {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h2 className="text-2xl">Manage Mechanic</h2>
+      <h2 className="text-2xl">Manage Mechanic:{mechanics.length}</h2>
     </div>
   );
 };
