01577eb9e760499eba335148bb6e667cb66ed04a
capmahin <mahinulhuque1996@gmail.com>
2022-06-25 06:04:09 +0600

    add useQueary

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import Loading from "../Shared/Loading";
 
 const Payment = () => {
   const { id } = useParams();
   const url = `http://localhost:5000/booking/${id}`;
+
+  const { data: order, isLoading } = useQuery(["booking", id], () =>
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <div>
       <h2 className="text-2xl text-purple-500"> Please Pay for: {id} </h2>
